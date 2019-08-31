1206a84bb32a24072e6cadce2f41e01a75309165
Jose Carlos Gutierrez <joseco48@gmail.com>
2019-08-31 04:04:50 -0400

    Completando lógica del juego

--- src/main/java/com/mdeis/goltdd/GameOfLife.java
+++ src/main/java/com/mdeis/goltdd/GameOfLife.java
@@ -3,6 +3,8 @@ package com.mdeis.goltdd;
 public class GameOfLife {
 
     private boolean[][] cells;
+    private int rows;
+    private int cols;
 
     public GameOfLife(int rows, int cols) {
         if (rows < 0 || rows >= Integer.MAX_VALUE)
@@ -10,6 +12,8 @@ public class GameOfLife {
         if (cols < 0 || cols >= Integer.MAX_VALUE)
             throw new IllegalArgumentException("Los valores de las columnas no pueden ser menor que cero o mayor o igual a " + Integer.MAX_VALUE);
         cells = new boolean[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
     }
 
     public void setCellStatus(int i, int j, boolean status) {
@@ -23,10 +27,73 @@ public class GameOfLife {
     }
 
     public void play(){
+        boolean[][] newCellStatus = new boolean[rows][cols];
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
-                cells[i][j] =false;
+
+                int numberOfNeighbors = countNeighbors(i, j);
+                newCellStatus[i][j] =
+                        isAlive(i,j) && (numberOfNeighbors == 2 || numberOfNeighbors == 3) ||
+                        !isAlive(i,j) && numberOfNeighbors == 3;
             }
         }
+        cells = newCellStatus;
+    }
+
+    private int countNeighbors(int i, int j){
+        int counter = 0;
+
+        int upPosition =  i - 1;
+        int downPosition =  i + 1;
+        boolean canGoUp = upPosition >= 0;
+        boolean canGoDown = downPosition < rows;
+
+        int leftPosition =  j - 1;
+        int rightPosition =  j + 1;
+        boolean canGoToLeft = leftPosition >= 0;
+        boolean canGoToRight = rightPosition < cols;
+
+        // CASO 1: i - 1, j - 1     CASO 2: i - 1 , j       CASO 3: i - 1, j + 1
+        // CASO 4:  i  , j - 1          CELL(i, j)          CASO 5:   i  , j + 1
+        // CASO 6: i + 1, j - 1     CASO 7: i + 1 , j       CASO 8: i + 1, j + 1
+
+        // Casos de la fila de Arriba
+        if(canGoUp) {
+            // CASO 1
+            if (canGoToLeft && isAlive(upPosition, leftPosition))
+            counter++;
+
+            // CASO 2
+            if (isAlive(upPosition, j))
+                counter++;
+
+            // CASO 3
+            if (canGoToRight && isAlive(upPosition, rightPosition))
+                counter++;
+        }
+        // CASO 4
+        if(canGoToLeft && isAlive(i, leftPosition))
+            counter++;
+
+        // CASO 5
+        if(canGoToRight && isAlive(i, rightPosition))
+            counter++;
+
+        // Casos para la fila de Arriba
+        if(canGoDown){
+            // CASO 6
+            if(canGoToLeft && isAlive(downPosition, leftPosition))
+                counter++;
+
+            // CASO 7
+            if(isAlive(downPosition, j))
+                counter++;
+
+            // CASO 8
+            if(canGoToRight && isAlive(downPosition, rightPosition))
+                counter++;
+        }
+
+        return counter;
     }
 }
